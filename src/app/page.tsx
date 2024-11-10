181dc5ade4d65bb1404e55fd51c3d5b0a2d3d83f
Rahaima-Shakeel <164835046+Rahaima-Shakeel@users.noreply.github.com>
2024-11-10 22:52:51 +0500

    portfolio with css and nextjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,133 @@
+'use client';
 import Image from "next/image";
+import { RiArrowRightDownLine, RiLinkedinBoxFill, RiMailLine } from "react-icons/ri";
+import React from 'react';
+import "./globals.css";
 
-export default function Home() {
+const Page = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div id="main">
+      <div id="nav">
+        <h3>0</h3>
+        <h3>Menu</h3>
+      </div>
+      <div id="intro">
+        <h1>
+          Rahaima Shakeel <br />
+          Biomedical Engineer
+        </h1>
+        <div id="place">
+          <h3>(Sindh, Pakistan)</h3>
+          <h4>
+            <RiArrowRightDownLine /> Scroll down
+          </h4>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div id="hero-image">
+        <Image id="pic" src="IMG-20230517-WA0021.png" alt="Profile" />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div id="about">
+        <h2>
+          Empowering Healthcare with a Foundation in Biomedical Engineering and a Future in AI Innovation.
+        </h2>
+        <div id="about-part2">
+          <h3>(About Me)</h3>
+        </div>
       </div>
-    </main>
+      <div id="work">
+        <div id="work1">
+          <h3>(What I Do)</h3>
+          <RiArrowRightDownLine />
+        </div>
+        <div id="work2">
+          <div className="work2-part">
+            <div className="elem">
+              <h4>
+                <b>1. Biomedical Engineering &amp; Healthcare Innovation</b>
+              </h4>
+              <p>
+                With a strong foundation in Biomedical Engineering, I have gained hands-on experience at Isra Hospital and NICVD Hospital in Hyderabad, where I worked with biomedical systems and healthcare technologies. My current placement with the PEC GIT Program at IBET, LUMHS, allows me to apply this knowledge practically and collaborate on impactful projects.
+              </p>
+            </div>
+            <div className="elem">
+              <h4>2. Front-End Development &amp; Digital Solutions</h4>
+              <p>
+                Proficient in HTML, CSS, Tailwind CSS, Next.js, and TypeScript, I specialize in creating modern, responsive web designs that enhance user experience. These skills are an asset in integrating healthcare technology with digital solutions, enabling innovative approaches to healthcare accessibility and user engagement.
+              </p>
+            </div>
+          </div>
+          <div className="work2-part">
+            <div className="elem">
+              <h4>3. Creative &amp; Visual Design</h4>
+              <p>
+                With expertise in Canva, I create visually engaging content for both professional and academic purposes. This skill allows me to convey complex information clearly and creatively, which is especially valuable in designing educational materials, branding elements, and assessments.
+              </p>
+            </div>
+            <div className="elem">
+              <h4>4. AI &amp; Future-Oriented Healthcare</h4>
+              <p>
+               Currently pursuing a certification in Cloud Applied Generative AI Engineering, I am expanding my knowledge of AI to bring transformative solutions to healthcare. My focus is on integrating AI with biomedical innovations to drive progress in patient care and healthcare technologies.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="skills">
+        <h2>Skills</h2>
+        <div className="skills-container">
+          {[
+            "Biomedical Engineering",
+            "Microsoft Office",
+            "Creative Design",
+            "Instructional & Lecturing Skills",
+            "Academic Assessment",
+            "Project Management",
+            "Strong Organizational Skills",
+            "Web Development",
+            "HTML",
+            "Next.js",
+            "TypeScript & JavaScript",
+            "CSS & Tailwind CSS",
+            "Canva",
+            "Leadership Skills",
+          ].map((skill) => (
+            <div className="skill" key={skill}>
+              <h3>{skill}</h3>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div id="contact">
+        <h2>Contact Me</h2>
+        <div className="contact-info">
+          <p>
+            Email:{" "}
+            <a href="mailto:rahaimasha2628@gmail.com?subject=Subject%20Here&body=Hello%2C%20I%20would%20like%20to%20discuss%20...">
+              rahaimasha2628@gmail.com
+            </a>
+          </p>
+          <p>
+            LinkedIn:{" "}
+            <a
+              href="https://www.linkedin.com/in/rahaima-shakeel-1a5b552b8/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn
+            </a>
+          </p>
+          <div id="icons">
+            <a href="https://www.linkedin.com/in/rahaima-shakeel-1a5b552b8/" target="_blank" rel="noopener noreferrer">
+              <RiLinkedinBoxFill />
+            </a>
+            <a href="mailto:rahaimasha2628@gmail.com">
+              <RiMailLine />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   );
-}
+};
+
+export default Page;
